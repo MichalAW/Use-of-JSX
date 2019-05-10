7951e8739a3e89e2742fa7f2a9785b94896a69f7
MichalAW <michal.wanta@gmail.com>
2019-05-10 21:56:11 +0200

    delete {}

--- components/Contact.js
+++ components/Contact.js
@@ -6,9 +6,9 @@ var Contact = React.createClass({
     render: function() {
         return (
             <div className='contactItem'>
-                <img className={'contactImage'} src={'images/contact.png'} alt={'contactImage'}></img>
-                <p className={'contactLabel'}>Name: {this.props.contact.firstName}</p>
-                <p className={'contactLabel'}>Surname: {this.props.contact.lastName}</p>
+                <img className='contactImage' src='images/contact.png' alt='contactImage'></img>
+                <p className='contactLabel'>Name: {this.props.contact.firstName}</p>
+                <p className='contactLabel'>Surname: {this.props.contact.lastName}</p>
                 <a href={'mailto:' + this.props.item.email}>{this.props.item.email}</a>
             </div>
         )
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,9 +6,9 @@ var ContactForm = React.createClass({
     render: function() {
         return (
             <form className='contactForm'>
-                <input type={'text'} placeholder={'Name'} id={'inputYourName'} required={''} value={this.props.contact.firstName}></input>
-                <input type={'text'} placeholder={'Surname'} id={'inputYourSurname'} required={''} value={this.props.contact.lastName}></input>
-                <input type={'email'} placeholder={'Email'} id={'inputYourEmail'} required={''} value={this.props.contact.email}></input>
+                <input type='text' placeholder='Name' id='inputYourName' required='' value={this.props.contact.firstName}></input>
+                <input type='text' placeholder='Surname' id='inputYourSurname' required='' value={this.props.contact.lastName}></input>
+                <input type='email' placeholder='Email' id='inputYourEmail' required='' value={this.props.contact.email}></input>
                 <button type='submit'>Add contact</button>
             </form>
         );
