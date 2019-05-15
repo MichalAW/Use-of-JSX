bdc58c41a2026fbebaeae0b2cfb05a168d65702a
MichalAW <michal.wanta@gmail.com>
2019-05-15 22:20:50 +0200

    fixed contactForm

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,36 +6,30 @@ var ContactForm = React.createClass({
     render: function() {
         return (
             <form className='contactForm'>
-                <label htmlFor='inputYourName'>
-                    Name:
-                    <input 
-                        type='text' 
-                        placeholder='Name' 
-                        id='inputYourName'
-                        required 
-                        value={this.props.contact.firstName}
-                    />
-                </label>
-                <label htmlFor='inputYourSurname'>
-                    Surname:
-                    <input 
-                        type='text' 
-                        placeholder='Surname' 
-                        id='inputYourSurname' 
-                        required
-                        value={this.props.contact.lastName}
-                    />
-                </label>
-                <label htmlFor='inputYourEmail'>
-                    Email:
-                    <input 
-                        type='email' 
-                        placeholder='Email' 
-                        id='inputYourEmail' 
-                        required
-                        value={this.props.contact.email}
-                    />
-                </label>
+                <label htmlFor='inputYourName'>Name:</label>
+                <input 
+                    type='text' 
+                    placeholder='Name' 
+                    id='inputYourName'
+                    required 
+                    value={this.props.contact.firstName}
+                />
+                <label htmlFor='inputYourSurname'>Surname:</label>
+                <input 
+                    type='text' 
+                    placeholder='Surname' 
+                    id='inputYourSurname' 
+                    required
+                    value={this.props.contact.lastName}
+                />
+                <label htmlFor='inputYourEmail'>Email:</label>
+                <input 
+                    type='email' 
+                    placeholder='Email' 
+                    id='inputYourEmail' 
+                    required
+                    value={this.props.contact.email}
+                />
                 <button type='submit'>Add contact</button>
             </form>
         );
