7ebdb9fdce89597c18c782315c4e7027b5aed03e
MichalAW <michal.wanta@gmail.com>
2019-05-15 06:26:56 +0200

    fix label

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,35 +6,35 @@ var ContactForm = React.createClass({
     render: function() {
         return (
             <form className='contactForm'>
-                <label
-                    htmlFor='inputYourName'
-                />Name
-                <input 
-                    type='text' 
-                    placeholder='Name' 
-                    id='inputYourName'
-                    required='' 
-                    value={this.props.contact.firstName}
-                />
-                <label
-                    htmlFor='inputYourSurname'
-                />Surname
-                <input 
-                    type='text' 
-                    placeholder='Surname' 
-                    id='inputYourSurname' required=''
-                    value={this.props.contact.lastName}
-                />
-                <label
-                    htmlFor='inputYourEmail'
-                />Email
-                <input 
-                    type='email' 
-                    placeholder='Email' 
-                    id='inputYourEmail' 
-                    required='' 
-                    value={this.props.contact.email}
-                />
+                <label htmlFor='inputYourName'>
+                    Name:
+                    <input 
+                        type='text' 
+                        placeholder='Name' 
+                        id='inputYourName'
+                        required='' 
+                        value={this.props.contact.firstName}
+                    />
+                </label>
+                <label htmlFor='inputYourSurname'>
+                    Surname:
+                    <input 
+                        type='text' 
+                        placeholder='Surname' 
+                        id='inputYourSurname' required=''
+                        value={this.props.contact.lastName}
+                    />
+                </label>
+                <label htmlFor='inputYourEmail'>
+                    Email:
+                    <input 
+                        type='email' 
+                        placeholder='Email' 
+                        id='inputYourEmail' 
+                        required='' 
+                        value={this.props.contact.email}
+                    />
+                </label>
                 <button type='submit'>Add contact</button>
             </form>
         );
